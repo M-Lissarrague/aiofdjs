3fa75f16c52533b9ff65d1d43c8785d95823e2c8
M-Lissarrague <manuel.lissarrague@outlook.com>
2026-04-15 14:07:59 +0200

    Add a prompt web page

---
+++ prompt.js
@@ -0,0 +1,9 @@
+let firstNumber = parseInt(prompt("Pick a number"));
+let secondNumber = parseInt(prompt("Pick another number"));
+let sum = firstNumber + secondNumber;
+alert(sum);
+console.log(
+  "The sum of " + firstNumber + " and " + secondNumber + " is " + sum,
+);
+console.log(`The sum of ${firstNumber} and ${secondNumber} is ${sum}`);
+console.log(typeof firstNumber);
